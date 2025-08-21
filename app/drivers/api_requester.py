db1e2a60cbdc6d98dde1262c36affde561f4da25
pedro.oliveira_onfly <pedro.oliveira@onfly.com.br>
2025-08-20 21:07:46 -0300

    Add driver request

---
+++ app/drivers/api_requester.py
@@ -0,0 +1,11 @@
+import requests
+
+
+class ApiRequester:
+
+    async def get_top1000_git_repositories(self):
+
+        token = {"Authorization": {f"token {2}"}}
+
+        response = requests.get("sua url", params)
+        pass
